624852fbca4a08ea36550b0721c315a941210110
chao920413 <675708945@qq.com>
2019-05-30 21:02:26 +0800

    springboot-1

---
+++ springboor/src/main/java/app/mytime.java
@@ -0,0 +1,15 @@
+package app;
+
+import java.util.Date;
+
+public class mytime {
+    private long time;
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+}
